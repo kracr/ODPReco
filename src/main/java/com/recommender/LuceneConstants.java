2c94e3cea84cdc1fb63df130cf5bf8c26f147f8f
Maleeha456 <maleeha18112@iiitd.ac.in>
2020-07-06 23:14:07 +0530

    del

--- src/main/java/com/recommender/LuceneConstants.java
+++
@@ -1,9 +0,0 @@
-package com.recommender;
-
-public class LuceneConstants {
-	 public static final String CONTENTS = "contents";
-	   public static final String FILE_NAME = "filename";
-	   public static final String FILE_PATH = "filepath";
-	   public static final int MAX_SEARCH = 5;
-
-}
